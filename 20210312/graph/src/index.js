e4584719d3840c0133d840e9e278e7f6e2804a99
Sebastien <sebastien.wagnieres@heig-vd.ch>
2021-04-12 23:14:12 +0200

    Exercices 12 Mars

--- 20210312/graph/src/index.js
+++ 20210312/graph/src/index.js
@@ -3,59 +3,96 @@ import {
   select,
   scaleLinear,
   max,
+  shape,
+  arc,
+  pie
 } from 'd3'
 
-const DATA = [
-  { nom: 'Lausanne', population: 138905 },
-  { nom: 'Yverdon-les-Bains', population: 30143 },
-  { nom: 'Montreux', population: 26574 },
-  { nom: 'Renens', population: 21036 },
-  { nom: 'Nyon', population: 20533 },
-  { nom: 'Vevey', population: 19827 },
+const DATA = [ 
+      {
+          "legende": "Lausanne",
+          "number": 326.86
+      },
+      {
+          "legende": "Yverdon-les-Bains",
+          "number": 125.72
+      },
+      {
+          "legende": "Ollon",
+          "number": 79.59
+      },
+      {
+          "legende": "Ecublens (VD)",
+          "number": 79.54
+      },
+      {
+          "legende": "Montreux",
+          "number": 75.73
+      }
 ]
 
 const WIDTH = 1000
 const HEIGHT = 500
-const MARGIN = 5
-const MARGIN_LEFT = 50
-const MARGIN_BOTTOM = 50
-const BAR_WIDTH = (WIDTH - MARGIN_LEFT) / DATA.length
+const container = DOM.svg(WIDTH, HEIGHT)
+const svg = d3.select(container)
 
-const svg = select('body')
-  .append('svg')
-  .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
+const arcCreator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(HEIGHT / 2 - 10)
 
-const yScale = scaleLinear()
-  .domain([0, max(DATA, d => d.population)])
-  .range([HEIGHT - MARGIN_BOTTOM, 0])
+const color = ({ DATA }) => {
+  switch (DATA.name){
+    case 'Lausanne': return 'gold'
+    case 'Yverdon-les-Bains': return 'blue'
+    case 'Ollon': return 'green'
+    case 'Ecublens (VD)': return 'orange'
+    case 'Montreux': return 'purple'
+  }
+}
 
+const pie = svg.append('g')
+.attr('transform', `translate(${HEIGHT / 2}, ${HEIGHT / 2})`)
 
-const g = svg.append('g')
-  .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
+pie.selectAll('path')
+.data(pieData)
+.enter()
+.append('path')
+.attr('d', arcCreator)
+.attr('fill', color)
 
-g.selectAll('rect')
-  .data(DATA)
-  .enter()
-  .append('rect')
-  .attr('x', (d, i) =>  i * BAR_WIDTH)
-  .attr('width', BAR_WIDTH - MARGIN)
-  .attr('y', d => yScale(d.population))
-  .attr('height', d => HEIGHT - MARGIN_BOTTOM - yScale(d.population))
-  .attr('fill', 'steelblue')
+  // un texte pour chaque tranche
+  pie.selectAll('text')
+    .data(pieData)
+    .enter()
+    .append('text')
+    // .centroid permet de trouver le centre de la tranche
+    .attr('transform', d => `translate(${arcCreator.centroid(d)})`)
+    .attr('text-anchor', 'middle')
+    .text(d => d.data.name)
 
-g.selectAll('text')
-  .data(DATA)
-  .enter()
-  .append('text')
-  .text(d => d.nom)
-  .attr('x', (d, i) =>  i * BAR_WIDTH + BAR_WIDTH / 2)
-  .attr('y', HEIGHT - MARGIN_BOTTOM / 2)
-  .attr('text-anchor', 'middle')
+      // la légende
+  const legend = svg.append('g')
+  .attr('transform', `translate(${HEIGHT-10})`)
 
-const axisY = axisLeft().scale(yScale)
-  .tickFormat(d => `${d / 1000}k`)
-  .ticks(5)
-
-svg.append('g')
-  .attr('transform', `translate(${MARGIN_LEFT - 3})`)
-  .call(axisY)
+  const RECT_WIDTH = 20
+  
+  // les carrés de couleur3.line()
+  legend.selectAll('rect')
+    .data(pieData)
+    .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * RECT_WIDTH)
+    .attr('width', RECT_WIDTH)
+    .attr('height', RECT_WIDTH)
+    .attr('fill', color)
+  
+  // les noms de fruits
+  legend.selectAll('text')
+    .data(pieData)
+    .enter()
+    .append('text')
+    .attr('x', RECT_WIDTH * 1.5)
+    .attr('y', (d, i) => i * RECT_WIDTH + RECT_WIDTH * 0.75)
+    .attr('width', RECT_WIDTH)
+    .attr('height', RECT_WIDTH)
+    .text(d => d.data.name)
