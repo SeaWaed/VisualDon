57350a01a49a3c2cf7f8109a124fadbf193768ed
Sebastien <sebastien.wagnieres@heig-vd.ch>
2021-04-23 15:41:56 +0200

    Exercice 3 du 23.04.2021

--- 20210423/carte-leaflet/src/index.js
+++ 20210423/carte-leaflet/src/index.js
@@ -4,9 +4,9 @@ import japan from './volcano.json'
 const map = L.map('map').setView([35.362799, 138.730781], 5)
 
 L.tileLayer(
-  'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
+  'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}',
   {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '<a href="https://stadiamaps.com/">Stadia Maps</a> <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     subdomains: 'abcd',
     minZoom: 0,
     maxZoom: 20,
@@ -28,12 +28,10 @@ japan.map(d => {
 L.geoJSON(
   japan,
   {
-    style: feature =>
-      feature.properties['name:fr'] === 'Mont Fuji'
-        ? { color: 'indianred' }
-        : { color: 'steelblue' },
+    style: features =>
+      features.properties['name'],
     onEachFeature: (feature, layer) =>
-      layer.bindPopup(feature.properties.name || feature.properties['volcano:status'] || feature.properties.uid)
+      layer.bindPopup(feature.properties.name || feature.properties['volcano:status'] || feature.id)
       
   },
 ).addTo(map)
--- 20210423/librairies-carto.md
+++ 20210423/librairies-carto.md
@@ -1,4 +1,5 @@
 **Pourquoi doit-on projeter des données cartographiques?**
+Cela permet de représenter une surface courbe (la Terre) sur une surface plane tout en contrôlant les déformations.
 
 **Qu'est ce qu'Open street map?**
 C'est un "wiki" de la map monde contenant des données géographique.
@@ -9,5 +10,12 @@ C'est un "wiki" de la map monde contenant des données géographique.
     3. geoTransform
 
 **À quoi sert le format topojson? En quoi est-il différent du geojson?**
+C'est une extension de GeoJson. Sa différence est qu'il prend beaucoup moins de place que ce dernier, mais pour la même quantité d'informations.
 
 **À quoi sert turf? Décrivez ce que font trois fonctions de cette libraire**
+C'est une série de fonction comme D3.
+
+ 3 Exemples de fonction:
+1. Distance = Permet de calculer la distance entre deux points
+2. Circle = Prend un point et calcule un cercle polygone. 
+3. union = Ppermet de mettre ensemble deux formes
